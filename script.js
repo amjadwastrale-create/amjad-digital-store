a7f36ee55b434e5f296dfdf2efedddcd79436e5a
amjadwastrale-create <amjadwastrale@gmail.com>
2026-07-09 21:52:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 console.log("Amjad Digital Store Loaded");
 alert("Welcome to Amjad Digital Store!");
+let cart = [];
+
+function addToCart(productName, price) {
+  cart.push({
+    name: productName,
+    price: price
+  });
+
+  alert(productName + " Cart me add ho gaya!");
+
+  console.log(cart);
+}
 
