6bd2f5407ae8957c1e9f5351dc6b28f9361684b1
amjadwastrale-create <amjadwastrale@gmail.com>
2026-07-09 22:46:54 +0530

    Update script.js
    Update script

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
-console.log("Amjad Digital Store Loaded");
-alert("Welcome to Amjad Digital Store!");
-let cart = [];
-
-function addToCart(productName, price) {
-  cart.push({
-    name: productName,
-    price: price
-  });
-
-  alert(productName + " Cart me add ho gaya!");
-
-  console.log(cart);
+function buyNow(productName) {
+    let phone = "9142956411";
+    let message = "Hello AMJAD DIGITAL STORE,%0A%0AI want to buy: " + productName;
+    window.open("https://wa.me/91" + phone + "?text=" + message, "_blank");
 }
 
+function searchProducts() {
+    let input = document.getElementById("search").value.toLowerCase();
+    let products = document.querySelectorAll(".product");
+
+    products.forEach(function(product) {
+        let name = product.querySelector("h3").innerText.toLowerCase();
+
+        if (name.includes(input)) {
+            product.style.display = "block";
+        } else {
+            product.style.display = "none";
+        }
+    });
+}
